11c20972c37d621a056035bfc2f60f4fa1fe1b64
anjit pariyar <anjitpariyar@gmail.com>
2023-03-10 22:16:08 +0545

    doc name added

--- php/add_new_customer.php
+++ php/add_new_customer.php
@@ -5,7 +5,7 @@
     $contact_number = $_GET["contact_number"];
     $address = ucwords($_GET["address"]);
     $doctor_name = ucwords($_GET["doctor_name"]);
-    $doctor_address = ucwords($_GET["doctor_address"]);
+    // $doctor_address = ucwords($_GET["doctor_address"]);
 
     $query = "SELECT * FROM customers WHERE CONTACT_NUMBER = '$contact_number'";
     $result = mysqli_query($con, $query);
@@ -13,7 +13,7 @@
     if($row)
       echo "Customer ".$row['NAME']." with contact number $contact_number already exists!";
     else {
-      $query = "INSERT INTO customers (NAME, CONTACT_NUMBER, ADDRESS, DOCTOR_NAME, DOCTOR_ADDRESS) VALUES('$name', '$contact_number', '$address', '$doctor_name', '$doctor_address')";
+      $query = "INSERT INTO customers (NAME, CONTACT_NUMBER, ADDRESS, DOCTOR_NAME) VALUES('$name', '$contact_number', '$address', '$doctor_name')";
       $result = mysqli_query($con, $query);
       if(!empty($result))
   			echo "$name added...";
